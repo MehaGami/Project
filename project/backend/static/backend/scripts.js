97342a78cf7abd651074cefdfedd6039adb59c60
MehaGami <vasilpol203@gmail.com>
2024-07-28 14:57:18 +0300

    create simple tittle

--- project/backend/static/backend/scripts.js
+++ project/backend/static/backend/scripts.js
@@ -1,3 +1,24 @@
+function validateForm() {
+
+    const name = document.getElementById('name').value.trim();
+    const password = document.getElementById('password').value.trim();
+    
+    const validName = "vasil";
+    const validPassword = "123321";
+    
+    if (name !== validName) {
+        document.getElementById('message').innerText = 'Name does not match';
+        return false;
+    }
+    if (password !== validPassword) {
+        document.getElementById('message').innerText = 'Password does not match';
+        return false;
+    }
+
+    document.getElementById('message').innerText = 'Successfully signed in!';
+    return true;
+}
+
 function fetchMessage() {
     fetch('/api/message/')
         .then(response => response.json())
